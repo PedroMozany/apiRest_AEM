96c31f34e72952e35a9189b789fdd36d2b3e6057
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2022-07-14 17:21:48 -0300

    removendo erros

--- src/main/java/controller/IAcao.java
+++ src/main/java/controller/IAcao.java
@@ -6,14 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface IAcao {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     String acao(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-=======
 
->>>>>>> 076c33dcf5707f8268efa0c21475e37211ea4d20
-=======
-
->>>>>>> origin/developer
 
 }
