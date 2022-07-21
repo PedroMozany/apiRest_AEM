7b461be9db3539811fd6fa307425baf4bb6fcdcb
PedroMozany <pedro.mozanny@hotmail.com>
2022-07-21 19:55:03 -0300

    configurações heruko

--- src/main/java/factory/ConectionFactory.java
+++ src/main/java/factory/ConectionFactory.java
@@ -10,20 +10,20 @@ import java.sql.SQLException;
 public class ConectionFactory {
     public DataSource dataSource;
 
-    public ConectionFactory(){
+    public ConectionFactory() {
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
-        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/redteam?useTimezone=true&serverTimezone=UTC");
-        comboPooledDataSource.setUser("root");
-        comboPooledDataSource.setPassword("admin123");
+        comboPooledDataSource.setJdbcUrl("jdbc:postgres://xuzotaszqmdsgo:15ec21679fd40d5d0af51e9825eb9e885578e5bccc865f627e01f89ffa1ae90f@ec2-34-192-210-139.compute-1.amazonaws.com:5432/d6v1aen4h4hkqe");
+        comboPooledDataSource.setUser("xuzotaszqmdsgo");
+        comboPooledDataSource.setPassword("15ec21679fd40d5d0af51e9825eb9e885578e5bccc865f627e01f89ffa1ae90f");
         comboPooledDataSource.setMaxPoolSize(15);
         this.dataSource = comboPooledDataSource;
     }
 
-    public Connection recuperarConexao()  throws SQLException, ClassNotFoundException, ConexaoException {
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    public Connection recuperarConexao() throws SQLException, ClassNotFoundException, ConexaoException {
+        try {
+            Class.forName("org.postgresql.Driver");
             return this.dataSource.getConnection();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             throw new ConexaoException("Erro ao fechar a conexão" + e);
         }
 
